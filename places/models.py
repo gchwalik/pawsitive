ea568a7c595efd5ec49e759c4d6e77a611285f02
gchwalik <89762294+gchwalik@users.noreply.github.com>
2025-05-09 18:01:57 -0700

    added basic Places class

---
+++ places/models.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+from django.db import models
+
+class Place(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
---
+++ places/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
+from .models import Place
+
+
+class PlaceListView(ListView):
+    model = Place
+    template_name = "places/index.html"
+    context_object_name = "places"
+
+    def get_queryset(self):
+        return Place.objects.all()
+
+# class PlaceDetailView(DetailView):
+#     model = Place
+#     template_name = "places/place_detail.html"
+#     context_object_name = "place"
+
+# class PlaceCreateView(CreateView):
+#     model = Place
+#     fields = ["name"]
+
+# class PlaceUpdateView(UpdateView):
+#     model = Place
+#     fields = ["name"]
+#     template_name_suffix = "_update_form"
+
+# class PlaceDeleteView(DeleteView):
+#     model = Place
+#     template_name = "places/place_confirm_delete.html"
+#     success_url = "/places/"
