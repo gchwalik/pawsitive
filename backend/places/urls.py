8da676f78454a746a3ee4d162454e7de113bd271
gchwalik <89762294+gchwalik@users.noreply.github.com>
2025-05-13 14:28:18 -0700

    appended slashed to project urls

--- backend/places/urls.py
+++ backend/places/urls.py
@@ -5,10 +5,10 @@ from . import views
 app_name = "places"
 urlpatterns = [
     path("", views.PlaceListView.as_view(), name="index"),
-    path("<int:pk>", views.PlaceDetailView.as_view(), name="place_read"),
-    path("create", views.PlaceCreateView.as_view(), name="place_create"),
-    path("<int:pk>/edit", views.PlaceUpdateView.as_view(), name="place_update"),
-    path("<int:pk>/delete", views.PlaceDeleteView.as_view(), name="place_delete"),
+    path("<int:pk>/", views.PlaceDetailView.as_view(), name="place_read"),
+    path("create/", views.PlaceCreateView.as_view(), name="place_create"),
+    path("<int:pk>/edit/", views.PlaceUpdateView.as_view(), name="place_update"),
+    path("<int:pk>/delete/", views.PlaceDeleteView.as_view(), name="place_delete"),
 ]
 
 
