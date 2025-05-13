7f723a621e90f4788fb76fda28b827254eca4a14
gchwalik <89762294+gchwalik@users.noreply.github.com>
2025-05-13 13:13:45 -0700

    adding type to the place class, incomplete

--- places/models.py
+++ places/models.py
@@ -7,6 +7,18 @@ class Place(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=200)
+    place_type = models.ForeignKey('PlaceType', blank=False, null=True ,on_delete=models.RESTRICT, related_name='places')
+
+    def __str__(self):
+        return f"Place(name={self.name}, type={ getattr(self,"place_type",None) })"
+
+
+class PlaceType(models.Model):
+    # Default id field automatically created
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=200, unique=True)
+    help_text = "Category of place: Park, Hiking Trail, etc.)"
 
     def __str__(self):
         return self.name
