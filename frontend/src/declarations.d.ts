292f026ee073c29f09071e63f880be291c07fe64
gchwalik <89762294+gchwalik@users.noreply.github.com>
2025-05-15 17:13:43 -0700

    incomplete tsx port

---
+++ frontend/src/declarations.d.ts
@@ -0,0 +1,11 @@
+// For assets (SVGs, CSS, etc.)
+declare module '*.svg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.css' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
