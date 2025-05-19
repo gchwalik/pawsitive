ca89cebfd8ff81a504b88b4dc865573a15f981bd
gchwalik <89762294+gchwalik@users.noreply.github.com>
2025-05-19 16:16:39 -0700

    drf tutorial pt3

--- backend/snippets/urls.py
+++ backend/snippets/urls.py
@@ -4,10 +4,10 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 
-app_name = "places"
+app_name = "snippets"
 urlpatterns = [
-    path('', views.snippet_list),
-    path('<int:pk>/', views.snippet_detail),
+    path('', views.SnippetList.as_view(), name='index'),
+    path('<int:pk>/', views.SnippetDetail.as_view(), name='detail'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
