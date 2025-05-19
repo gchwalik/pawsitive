5ebac4022d401650cf5ae966af1baf9549865a85
gchwalik <89762294+gchwalik@users.noreply.github.com>
2025-05-19 11:30:08 -0700

    set up and config'd react-ts with pandu's help

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,15 +4,15 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState<number>(0)
+  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank" rel="noopener noreferrer">
+        <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
+        <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
