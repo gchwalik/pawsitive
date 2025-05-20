dea6cd143e5d52b739ba5fcd2d84801f33623db8
gchwalik <89762294+gchwalik@users.noreply.github.com>
2025-05-19 19:17:20 -0700

    drf tutorial pt5, before adding in hyperlinks

--- backend/snippets/urls.py
+++ backend/snippets/urls.py
@@ -6,10 +6,12 @@ from snippets import views
 
 app_name = "snippets"
 urlpatterns = [
-    path('', views.SnippetList.as_view(), name='index'),
-    path('<int:pk>/', views.SnippetDetail.as_view(), name='detail'),
-    path('users/', views.UserList.as_view()),
-    path('users/<int:pk>/', views.UserDetail.as_view()),
+    path('', views.api_root),
+    path('index/', views.SnippetList.as_view(), name='snippet_list'),
+    path('<int:pk>/', views.SnippetDetail.as_view(), name='snippet_detail'),
+    path('<int:pk>/highlight/', views.SnippetHighlight.as_view(), name='snippet_highlight'),
+    path('users/', views.UserList.as_view(), name='user_list'),
+    path('users/<int:pk>/', views.UserDetail.as_view(), name='user_detail'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- backend/snippets/views.py
+++ backend/snippets/views.py
@@ -2,10 +2,11 @@ from django.contrib.auth.models import User
 from django.http import HttpResponse, JsonResponse, Http404
 from django.views.decorators.csrf import csrf_exempt
 
-from rest_framework import generics, mixins, permissions, status
+from rest_framework import generics, mixins, permissions, renderers, status
 from rest_framework.decorators import api_view
 from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
+from rest_framework.reverse import reverse
 from rest_framework.views import APIView
 
 from snippets.models import Snippet
@@ -29,6 +30,22 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
         permissions.IsAuthenticatedOrReadOnly,
         IsOwnerOrReadOnly,
     ]
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('snippets:user_list', request=request, format=format),
+        'snippets': reverse('snippets:snippet_list', request=request, format=format)
+    })
 
 
 class UserList(generics.ListAPIView):
