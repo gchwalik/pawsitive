e7a3d97b3a7d7c43ffc7402aba2073afc319b1b8
gchwalik <89762294+gchwalik@users.noreply.github.com>
2025-05-29 13:53:49 -0700

    cleanup

--- frontend/src/components/Container.tsx
+++ frontend/src/components/Container.tsx
@@ -6,20 +6,18 @@ interface ContainerProps {
 }
 
 function Container({ title, showTitleBorder = false, minHeight = true, children }: ContainerProps) {
-  title = title ? title : "";
+  title = title ? title : "Error";
   return (
-    <>
-      <div className={`w-full sm:w-3/4 md:w-2/3 lg:w-1/2 bg-fuchsia-50 pt-1 rounded-lg mt-5 shadow-lg ${minHeight ? `min-h-70` : ""}`}>
-        <div className="flex flex-col h-full">
-          {title && (
-            <h2 className={`text-center text-xl font-medium p-3 ${showTitleBorder ? 'border-b-1 border-neutral-500' : ""}`}>
-              {title}
-            </h2>
-          )}
-          {children}
-        </div>
+    <div className={`w-full sm:w-3/4 md:w-2/3 lg:w-1/2 bg-fuchsia-50 pt-1 rounded-lg mt-5 shadow-lg ${minHeight ? `min-h-70` : ""}`}>
+      <div className="flex flex-col h-full">
+        {title && (
+          <h2 className={`text-center text-xl font-medium p-3 ${showTitleBorder ? 'border-b-1 border-neutral-500' : ""}`}>
+            {title}
+          </h2>
+        )}
+        {children}
       </div>
-    </>  
+    </div>
   );
 }
 
