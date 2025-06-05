279dae8e3c4f252dbe2b4853a7ae1024ce3375ef
gchwalik <89762294+gchwalik@users.noreply.github.com>
2025-06-05 15:20:58 -0700

    oops, forgot to remove the ordering, when removing the ordering migration

--- backend/places/models.py
+++ backend/places/models.py
@@ -6,8 +6,5 @@ class Place(models.Model):
     updated = models.DateTimeField(auto_now=True, editable=False) # added editable=False for clarity
     name = models.CharField(max_length=200)
 
-    class Meta:
-        ordering = ['name']
-
     def __str__(self):
         return self.name
