41204bc93df802c378524642dacd3a516769c4c2
gchwalik <89762294+gchwalik@users.noreply.github.com>
2025-06-05 14:52:34 -0700

    hm

--- backend/places/urls.py
+++ backend/places/urls.py
@@ -3,14 +3,6 @@ from rest_framework.routers import DefaultRouter
 
 from places import views
 
-# app_name = "places"
-# urlpatterns = [
-#     path("", views.PlaceListView.as_view(), name="index"),
-#     path("<int:pk>/", views.PlaceDetailView.as_view(), name="place_read"),
-#     path("create/", views.PlaceCreateView.as_view(), name="place_create"),
-#     path("<int:pk>/edit/", views.PlaceUpdateView.as_view(), name="place_update"),
-#     path("<int:pk>/delete/", views.PlaceDeleteView.as_view(), name="place_delete"),
-# ]
 router = DefaultRouter()
 router.register(r'places', views.PlaceViewSet, basename='place')
 
@@ -18,12 +10,3 @@ router.register(r'places', views.PlaceViewSet, basename='place')
 urlpatterns = [
     path('', include(router.urls)),
 ]
-
-
-"""
-view places list
-create new place 
-edit place
-delete place
-
-"""
--- backend/places/views.py
+++ backend/places/views.py
@@ -1,9 +1,3 @@
-from django.shortcuts import render
-from django.urls import reverse_lazy
-from django.utils.decorators import method_decorator
-from django.views.decorators.csrf import csrf_protect
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-
 from rest_framework import viewsets
 
 from .models import Place
@@ -17,42 +11,3 @@ class PlaceViewSet(viewsets.ModelViewSet):
     """
     queryset = Place.objects.all()
     serializer_class = PlaceSerializer
-
-
-# @method_decorator(csrf_protect, name='dispatch')
-# class PlaceListView(ListView):
-#     model = Place
-#     template_name = "places/index.html"
-#     context_object_name = "places"
-
-# class PlaceDetailView(DetailView):
-#     model = Place
-
-# class PlaceCreateView(CreateView):
-#     model = Place
-#     fields = ["name"]
-#     success_url = reverse_lazy('places:index')
-
-#     def get_context_data(self, **kwargs):
-#         # Call the base implementation first to get a context
-#         context = super().get_context_data(**kwargs)
-#         context["page_title"] = "Create Place"
-#         context["btn_txt"] = "Create"
-#         return context
-
-# class PlaceUpdateView(UpdateView):
-#     model = Place
-#     fields = ["name"]
-#     success_url = reverse_lazy('places:index')
-
-#     def get_context_data(self, **kwargs):
-#         # Call the base implementation first to get a context
-#         context = super().get_context_data(**kwargs)
-#         context["page_title"] = "Update Place"
-#         context["btn_txt"] = "Update"
-#         return context
-
-# class PlaceDeleteView(DeleteView):
-#     model = Place
-#     template_name = "places/place_confirm_delete.html"
-#     success_url = reverse_lazy('places:index')
