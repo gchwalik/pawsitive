b9193a2f626b9ebe709c218c8f4ffdea98e16cc0
gchwalik <89762294+gchwalik@users.noreply.github.com>
2025-06-05 15:26:26 -0700

    updated models, added placetype

--- backend/places/models.py
+++ backend/places/models.py
@@ -1,10 +1,23 @@
-from django.db.models import CharField, DateTimeField, Model
+from django.db.models import Model, CharField, DateTimeField, ForeignKey, PROTECT
 
 class Place(Model):
     # Default id field automatically created
     created = DateTimeField(auto_now_add=True, editable=False) # added editable=False for clarity
     updated = DateTimeField(auto_now=True, editable=False) # added editable=False for clarity
     name = CharField(max_length=200)
+
+    type = ForeignKey("PlaceType", on_delete=PROTECT, related_name="places", related_query_name="place", null=True)
+
+    def __str__(self):
+        return self.name
+
+class PlaceType(Model):
+    # Default id field automatically created
+    created = DateTimeField(auto_now_add=True, editable=False) # added editable=False for clarity
+    updated = DateTimeField(auto_now=True, editable=False) # added editable=False for clarity
+    name = CharField(max_length=200)
+
+    # many-to-one reference to Place
 
     def __str__(self):
         return self.name
