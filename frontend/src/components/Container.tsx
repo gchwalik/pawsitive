0bd382fde6e12957b0006918c1ee48d29dd5ebf6
gchwalik <89762294+gchwalik@users.noreply.github.com>
2025-06-06 10:28:01 -0700

    margin top

--- frontend/src/components/Container.tsx
+++ frontend/src/components/Container.tsx
@@ -8,7 +8,7 @@ interface ContainerProps {
 function Container({ title, showTitleBorder = false, minHeight = true, children }: ContainerProps) {
   title = title ? title : "";
   return (
-    <div className={`w-full sm:w-3/4 md:w-2/3 lg:w-1/2 bg-amber-50 rounded-lg mt-6 pt-1 shadow-lg ${minHeight ? `min-h-70` : ""}`}>
+    <div className={`w-full sm:w-3/4 md:w-2/3 lg:w-1/2 bg-amber-50 rounded-lg mt-15 pt-1 shadow-lg ${minHeight ? `min-h-70` : ""}`}>
       <div className="flex flex-col h-full">
         {title && (
           <h2 className={`text-center text-xl font-medium p-3 ${showTitleBorder ? 'border-b-1 border-neutral-500' : ""}`}>
