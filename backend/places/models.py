e61d788f6c228e73fd6c7160f7a26a9c6df2adbe
gchwalik <89762294+gchwalik@users.noreply.github.com>
2025-06-05 18:05:27 -0700

    place type (#60)
    * place type
    
    * newline
    
    * added place type
    
    * pagination
    
    * fixed test data typo
    
    * comment

--- backend/places/models.py
+++ backend/places/models.py
@@ -1,10 +1,25 @@
-from django.db import models
+from django.db.models import Model, CharField, DateTimeField, ForeignKey, PROTECT
+from django.core.exceptions import ValidationError
 
-class Place(models.Model):
+class Place(Model):
     # Default id field automatically created
-    created = models.DateTimeField(auto_now_add=True, editable=False) # added editable=False for clarity
-    updated = models.DateTimeField(auto_now=True, editable=False) # added editable=False for clarity
-    name = models.CharField(max_length=200)
+    created = DateTimeField(auto_now_add=True, editable=False) # added editable=False for clarity
+    updated = DateTimeField(auto_now=True, editable=False) # added editable=False for clarity
+    name = CharField(max_length=200)
+
+    type = ForeignKey("PlaceType", on_delete=PROTECT, related_name="places", related_query_name="place")
+
+    def __str__(self):
+        return self.name
+
+
+class PlaceType(Model):
+    # Default id field automatically created
+    created = DateTimeField(auto_now_add=True, editable=False) # added editable=False for clarity
+    updated = DateTimeField(auto_now=True, editable=False) # added editable=False for clarity
+    name = CharField(max_length=200)
+
+    # many-to-one reference to Place
 
     def __str__(self):
         return self.name
--- backend/places/serializers.py
+++ backend/places/serializers.py
@@ -1,7 +1,33 @@
 from rest_framework import serializers
-from places.models import Place
+from places.models import Place, PlaceType
+
+
+# For nested use in Place
+class PlaceTypeMinimalSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PlaceType
+        fields = ['id', 'name']
+
 
 class PlaceSerializer(serializers.ModelSerializer):
+    type = PlaceTypeMinimalSerializer(read_only=True)
+    type_id = serializers.PrimaryKeyRelatedField(
+        queryset=PlaceType.objects.all(),
+        source='type',
+        write_only=True
+    )
+
     class Meta:
         model = Place
-        fields = ['id', 'name', 'created', 'updated']
+        fields = ['id', 'name', 'type', 'type_id', 'created', 'updated']
+
+
+class PlaceTypeSerializer(serializers.ModelSerializer):
+    place_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = PlaceType
+        fields = ['id', 'name', 'created', 'updated', 'place_count']
+
+    def get_place_count(self, obj):
+        return obj.places.count()
