040718519f912a62e98742851e33cf11ab62000a
gchwalik <89762294+gchwalik@users.noreply.github.com>
2025-06-06 10:33:10 -0700

    font weight

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -7,7 +7,7 @@ function Header() {
     <nav className="navbar">
       <Link to="/" className="flex">
         <CompassIcon className="text-4xl font-semibold mr-0.5" weight="light" />
-        <h1 className="text-3xl font-medium ">
+        <h1 className="text-3xl">
           Pawsitive
         </h1>
       </Link>
