d6f54cfff4581d6530c04fe68d4bc926decdf720
gchwalik <89762294+gchwalik@users.noreply.github.com>
2025-06-07 10:23:22 -0700

    fonts (#68)
    * fonts
    
    * undoing testing change
    
    * adjust icon to header title
    
    * added font-medium to .label class

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -7,8 +7,8 @@ function Header() {
   return (
     <nav className="navbar">
       <Link to="/" className="flex">
-        <CompassIcon className="text-4xl font-semibold mr-0.5" weight="light" />
-        <h1 className="text-3xl">
+        <CompassIcon className="text-4xl font-semibold mr-0.75" weight="light" />
+        <h1 className="text-3xl font-medium">
           Pawsitive
         </h1>
       </Link>
