70e24afb6d207235dcc71dcd0887492d61a9f6b1
gchwalik <89762294+gchwalik@users.noreply.github.com>
2025-06-07 10:18:59 -0700

    adjust icon to header title

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -7,7 +7,7 @@ function Header() {
   return (
     <nav className="navbar">
       <Link to="/" className="flex">
-        <CompassIcon className="text-4xl font-semibold mr-0.5" weight="light" />
+        <CompassIcon className="text-4xl font-semibold mr-0.75" weight="light" />
         <h1 className="text-3xl font-medium">
           Pawsitive
         </h1>
