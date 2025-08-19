2b5eb3806d8fc9ce0ab9336ec6269a5d44e3e294
gchwalik <89762294+gchwalik@users.noreply.github.com>
2025-08-18 18:35:23 -0700

    create now redirects like it should (#91)

--- frontend/src/pages/PlaceCreatePage.tsx
+++ frontend/src/pages/PlaceCreatePage.tsx
@@ -1,16 +1,34 @@
+import { useNavigate } from 'react-router';
+import type { SubmitHandler } from 'react-hook-form';
+
 import Header from '../components/Header';
+import { CreateForm, FormInput } from '../components/form/EntityForm';
 
 import { getEmptyPlaceInput, createPlace } from '../api/placesApi';
-import { CreateForm, FormInput } from '../components/form/EntityForm';
 import type { PlaceInput } from '../api/placesApi';
 
+import { ROUTES } from '../routes';
+
 function CreatePlace() {
+  const navigate = useNavigate();
+
+  const handleCreate: SubmitHandler<PlaceInput> = async (placeInput: PlaceInput) => {
+    try {
+      const response = createPlace(placeInput);
+      console.log('Place created:', response);
+      // After successful creation, navigate back
+      navigate(ROUTES.FRONTEND.ROOT);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
   return (
     <>
       <Header />
       <CreateForm containerTitle='Create Place'
         defaultValues={getEmptyPlaceInput()}
-        onSubmit={createPlace}
+        onSubmit={handleCreate}
       >
         {(form) => (
           <>
