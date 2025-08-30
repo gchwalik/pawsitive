7597cd41d45d826dd233186b7de9edeeaececc15
gchwalik <89762294+gchwalik@users.noreply.github.com>
2025-08-30 12:18:12 -0700

    can now add className styling to ButtonContainer (#134)
    * can now add className styling to ButtonContainer
    
    * added back removed style class
    
    * lint

--- frontend/src/components/Buttons.tsx
+++ frontend/src/components/Buttons.tsx
@@ -1,10 +1,13 @@
 interface ButtonContainerProps {
+  className?: string;
   children?: React.ReactNode; // This receives whatever is between the tags
 }
 
-function ButtonContainer({ children }: ButtonContainerProps) {
+function ButtonContainer({ className = "", children }: ButtonContainerProps) {
   return (
-    <div className="flex justify-center items-center gap-4 mt-auto">
+    <div
+      className={`flex justify-center items-center gap-4 mt-auto ${className}`}
+    >
       {children}
     </div>
   );
