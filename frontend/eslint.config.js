5b56b51f4ed4da189edc46f301c91ad50f998c42
Dieu Huynh <dieu_huynh@apple.com>
2025-08-30 17:22:30 -0700

    add more React coverage to linting

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,6 +1,7 @@
 import js from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
 import importPlugin from "eslint-plugin-import";
+import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import globals from "globals";
@@ -20,12 +21,16 @@ export default tseslint.config(
       },
     },
     plugins: {
+      react,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
       import: importPlugin,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      "react/jsx-key": "error",
+      "react/react-in-jsx-scope": "off",
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
@@ -73,5 +78,5 @@ export default tseslint.config(
       },
     },
   },
-  eslintConfigPrettier,
+  eslintConfigPrettier
 );
