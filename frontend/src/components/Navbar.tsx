d5cb70980c2e31c45f4c9027efc758fe367d0696
gchwalik <89762294+gchwalik@users.noreply.github.com>
2025-09-05 14:16:07 -0700

    using menu instead of rolling my own

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,12 +1,24 @@
-import { CompassIcon } from "@phosphor-icons/react";
+import { CompassIcon, CaretDownIcon } from "@phosphor-icons/react";
 
 import AppLink from "./AppLink";
 import { Dropdown, DropdownLink } from "./Dropdown";
+import { Link } from "react-router";
+import {
+  Button,
+  ListBox,
+  ListBoxItem,
+  MenuTrigger,
+  Popover,
+  Select,
+  SelectValue,
+  Menu,
+  MenuItem
+} from "react-aria-components";
 
 function Navbar() {
   return (
     <nav className="navbar">
-      <AppLink to="/" className="flex">
+      <AppLink to="/" className="flex ml-auto flex-1">
         <CompassIcon
           className="text-4xl font-semibold mr-0.75"
           weight="light"
@@ -14,11 +26,23 @@ function Navbar() {
         <h1 className="text-3xl font-medium">Pawsitive</h1>
       </AppLink>
 
-      <div className="ml-auto">
-        <Dropdown label="More">
-          <DropdownLink to="#">Place Types</DropdownLink>
-        </Dropdown>
-      </div>
+      <MenuTrigger >
+        <Button className="dropdown-button w-30" aria-label="Menu">
+          <span className="flex-1">More</span>
+          <span aria-hidden="true">
+            <CaretDownIcon className="ml-1" size={16} />
+          </span>
+        </Button>
+        <Popover className="dropdown-popover w-40" placement="bottom end">
+          <Menu className="flex flex-col gap-1">
+            <MenuItem className="px-3 py-1 dropdown-element">
+              <Link to="/place-types" className="block w-full">
+                Place Types
+              </Link>
+            </MenuItem>
+          </Menu>
+        </Popover>
+      </MenuTrigger>
     </nav>
   );
 }
